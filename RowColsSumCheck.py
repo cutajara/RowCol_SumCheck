fb956647e404f43f428355d4bd5f778a28eda460
cutajarRMR <91999462+cutajarRMR@users.noreply.github.com>
2022-01-27 20:57:08 +1100

    Vectorized function

--- RowColsSumCheck.py
+++ RowColsSumCheck.py
@@ -1,16 +1,14 @@
 # Fusion File Checking: Checks Dataframe from any Rows or Columns that are empty
 
 def RowColCheck(df):
-    # Check each row has data
-    for i in range(0, df.shape[0]):
-        if sum(df.iloc[i,:].values.tolist()) == 0:
-            print("Warning, Zero Sum Respondnet: " + str(df.index[i]))
+    zerosrows = (df.sum(axis=1)==0)[(df.sum(axis=1)==0) == True] # Rows that sum == 0 
+    if len(zerosrows) > 0: # If there are rows that sum == 0
+        print("Warning, Zero Sum Respondnet:")
+        print(zerosrows) # Print rows that sum == 0
+    
+    zeroscols = (df.sum(axis=0)==0)[(df.sum(axis=0)==0) == True] # Columns that sum == 0
+    if len(zeroscols) > 0: # If there are columns that sum == 0
+        print("Warning, Zero Sum Column:")
+        print(zeroscols) # Print columns that sum == 0
             
-    # Check each column has data
-    colx = df.columns
-    for i in range(0, df.shape[1]):
-        if sum(df.iloc[:,i].values.tolist()) == 0:
-            print("Warning, Zero Sum Column: " + str(df.columns[i]))
-            
-            
-RowColCheck(df)
+           
